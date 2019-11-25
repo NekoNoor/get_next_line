31906babe7037068f639777c6c7e66967e8e2366
Noor Schat <nschat@student.codam.nl>
2019-11-25 20:52:56 +0100

    feat: add bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line_bonus.c                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nschat <nschat@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/25 20:51:45 by nschat        #+#    #+#                 */
+/*   Updated: 2019/11/25 20:51:48 by nschat        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static void		free_recursive(t_list *lst, int fd)
+{
+	t_list	*node;
+
+	if (lst->next->fd == fd)
+	{
+		node = lst->next;
+		lst->next = lst->next->next;
+		free(node->buf);
+		free(node);
+	}
+	else
+		free_recursive(lst->next, fd);
+}
+
+static void		free_buffer(t_list **alst, int fd)
+{
+	if ((*alst)->fd == fd)
+	{
+		free((*alst)->buf);
+		free(*alst);
+		*alst = NULL;
+	}
+	else
+		free_recursive(*alst, fd);
+}
+
+static ssize_t	read_line(t_list *file, char **out)
+{
+	ssize_t	ret;
+	size_t	i;
+
+	if (file->size == 0)
+	{
+		ret = read(file->fd, file->buf, BUFFER_SIZE);
+		if (ret == eof_read || ret == error)
+			return (ret);
+		file->buf[ret] = '\0';
+		file->size = ret;
+		return (loop);
+	}
+	i = get_index(file->buf, '\n');
+	*out = copy_buffer(*out, file, i);
+	if (*out == NULL)
+		return (error);
+	if (i < file->size)
+	{
+		ft_strncpy(file->buf, file->buf + i + 1, file->size - i - 1);
+		file->size = file->size - i - 1;
+		return (line_read);
+	}
+	else
+		file->size = 0;
+	return (loop);
+}
+
+int				get_next_line(int fd, char **line)
+{
+	static t_list	*buffers;
+	t_list			*file;
+	char			*out;
+	ssize_t			state;
+
+	file = get_buffer(&buffers, fd);
+	if (file == NULL)
+		return (error);
+	out = NULL;
+	state = loop;
+	while (state == loop)
+		state = read_line(file, &out);
+	if (state == line_read)
+		*line = out;
+	else
+		free_buffer(&buffers, fd);
+	return (state);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line_bonus.h                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nschat <nschat@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/25 20:51:52 by nschat        #+#    #+#                 */
+/*   Updated: 2019/11/25 20:51:53 by nschat        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+enum			e_retval
+{
+	error = -1,
+	eof_read = 0,
+	line_read = 1,
+	loop = 2
+};
+
+typedef struct	s_list
+{
+	char			*buf;
+	size_t			size;
+	int				fd;
+	struct s_list	*next;
+}				t_list;
+
+int				get_next_line(int fd, char **line);
+
+t_list			*get_buffer(t_list **alst, int fd);
+size_t			get_index(char *s, char c);
+char			*ft_strncpy(char *dst, const char *src, size_t n);
+char			*copy_buffer(char *old, t_list *file, size_t newl);
+
+#endif
